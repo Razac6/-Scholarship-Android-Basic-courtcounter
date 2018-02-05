6e810d70a94f4220fedd6a8c54285ca4bf263724
marcin <binekmarcin@gmail.com>
2018-02-05 18:20:56 +0100

    add some files

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,70 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForButtonThree(View v) {
+        displayForTeamA(scoreTeamA = scoreTeamA + 3);
+    }
+
+    public void displayForButtonTwo(View v) {
+        displayForTeamA(scoreTeamA = scoreTeamA + 2);
+    }
+
+    public void displayForButtonOne(View v) {
+        displayForTeamA(scoreTeamA = scoreTeamA + 1);
+    }
+
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForButtonBThree(View v) {
+        displayForTeamB(scoreTeamB = scoreTeamB + 3);
+    }
+
+    public void displayForButtonBTwo(View v) {
+        displayForTeamB(scoreTeamB = scoreTeamB + 2);
+    }
+
+    public void displayForButtonBOne(View v) {
+        displayForTeamB(scoreTeamB = scoreTeamB + 1);
+    }
+
+
+    /**
+     * Reset button
+     */
+    public void resetScoreButton(View v) {
+        displayForTeamA(scoreTeamA = 0);
+        displayForTeamB(scoreTeamB = 0);
+
+    }
+
+
 }
