6c5ddd93374de8e4373b2d921dcaffb2aa0d9382
marcin <binekmarcin@gmail.com>
2018-06-10 16:50:22 +0200

    change grid to two section

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,7 +8,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int cardTeamA = 0;
+
     int scoreTeamB = 0;
+    int cardTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
-    }
+
 
 //    public void displayForButtonThree(View v) {
 //        if (scoreTeamA > 0) {
@@ -37,19 +37,49 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
     /*
-     * Gool button teamA
+     * TeamA
      * */
-    public void displayForButtonOne(View v) {
-            displayForTeamA(scoreTeamA = scoreTeamA + 1);
+
+//    score
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 
+    public void displayForButtonOne(View v) {
+        displayForTeamA(scoreTeamA = scoreTeamA + 1);
+    }
+
+    //      cards
+    public void displayForACard(int card) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_cards);
+        scoreView.setText(String.valueOf(card));
+    }
+
+    public void displayForButtonAcard(View view) {
+        displayForACard(cardTeamA = cardTeamA + 1);
+    }
 
     /**
-     * Displays the given score for Team B.
+     * TeamB
      */
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForButtonBSecond(View v) {
+        displayForTeamB(scoreTeamB = scoreTeamB + 1);
+    }
+
+    //      cards
+    public void displayForBCard(int card) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_cards);
+        scoreView.setText(String.valueOf(card));
+    }
+
+    public void displayForButtonBcard(View view) {
+        displayForBCard(cardTeamB = cardTeamB + 1);
     }
 
 //    public void displayForButtonBThree(View v) {
@@ -64,21 +94,12 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
-    /*
-     * Gool button teamB
-     * */
-    public void displayForButtonBSecond(View v) {
-            displayForTeamB(scoreTeamB = scoreTeamB + 1);
-    }
-
-
     /**
      * Reset button
      */
     public void resetScoreButton(View v) {
         displayForTeamA(scoreTeamA = 0);
         displayForTeamB(scoreTeamB = 0);
-
     }
 
 
