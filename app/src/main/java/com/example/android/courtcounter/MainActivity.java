b668c2e5f2cf7624e29be1a7c2b4d5329e203cd2
marcin <binekmarcin@gmail.com>
2018-06-10 16:07:21 +0200

    changed method

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,8 +7,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 100;
-    int scoreTeamB = 100;
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,56 +20,64 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    public void displayForButtonThree(View v) {
-        if(scoreTeamA > 0){
-        displayForTeamA(scoreTeamA = scoreTeamA - 3);
-    }}
+//    public void displayForButtonThree(View v) {
+//        if (scoreTeamA > 0) {
+//            displayForTeamA(scoreTeamA = scoreTeamA - 3);
+//        }
+//    }
+//
+//    public void displayForButtonTwo(View v) {
+//        if (scoreTeamA > 0) {
+//            displayForTeamA(scoreTeamA = scoreTeamA - 2);
+//        }
+//    }
 
-    public void displayForButtonTwo(View v) {
-        if(scoreTeamA > 0){
-        displayForTeamA(scoreTeamA = scoreTeamA - 2);
-    }}
-
+    /*
+     * Gool button teamA
+     * */
     public void displayForButtonOne(View v) {
-        if(scoreTeamA > 0){
-        displayForTeamA(scoreTeamA = scoreTeamA - 1);
-    }}
+            displayForTeamA(scoreTeamA = scoreTeamA + 1);
+    }
 
 
     /**
      * Displays the given score for Team B.
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    public void displayForButtonBThree(View v) {
-        if(scoreTeamB > 0){
-        displayForTeamB(scoreTeamB = scoreTeamB - 3);
-    }}
+//    public void displayForButtonBThree(View v) {
+//        if (scoreTeamB > 0) {
+//            displayForTeamB(scoreTeamB = scoreTeamB - 3);
+//        }
+//    }
+//
+//    public void displayForButtonBTwo(View v) {
+//        if (scoreTeamB > 0) {
+//            displayForTeamB(scoreTeamB = scoreTeamB - 2);
+//        }
+//    }
 
-    public void displayForButtonBTwo(View v) {
-        if(scoreTeamB > 0){
-        displayForTeamB(scoreTeamB = scoreTeamB - 2);
-    }}
-
-    public void displayForButtonBOne(View v) {
-        if(scoreTeamB > 0){
-        displayForTeamB(scoreTeamB = scoreTeamB - 1);
-    }}
+    /*
+     * Gool button teamB
+     * */
+    public void displayForButtonBSecond(View v) {
+            displayForTeamB(scoreTeamB = scoreTeamB + 1);
+    }
 
 
     /**
      * Reset button
      */
     public void resetScoreButton(View v) {
-        displayForTeamA(scoreTeamA = 100);
-        displayForTeamB(scoreTeamB = 100);
+        displayForTeamA(scoreTeamA = 0);
+        displayForTeamB(scoreTeamB = 0);
 
     }
 
